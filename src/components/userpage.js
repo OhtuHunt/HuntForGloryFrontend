1730e7f8f6661822e2d811ee73b951015b10f178
Ouzii <oskari.laaja@gmail.com>
2018-02-19 11:50:17 +0200

    Fixed typo on userpage

--- src/components/userpage.js
+++ src/components/userpage.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Userpage = () => {
     return (
         <div>
-            <h2>Hello Henri!</h2>
+            <h2>Hello Henrik!</h2>
             <p>This is your personal page for settings etc. upcoming on spring 4</p>
         </div>
     )
